36007acf10d784ad6771c064600512b05852cc73
GengXuecheng <gengxuecheng@GengXuechengdeMacBook-Air.local>
2016-08-27 17:57:11 +0800

    test2

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-
+import unittest
 browser=webdriver.Firefox()
 browser.get("http://localhost:8000")
-assert 'Django' in browser.title
+print(browser.title)
+browser.quit()
